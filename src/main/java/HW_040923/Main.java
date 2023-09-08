4e1a13271d4ff77201fa465b500390aca15fd1a1
sulida <sulida2011@gmail.com>
2023-09-08 13:20:12 +0300

    Home Work 040923 - changed

--- src/main/java/HW_040923/Main.java
+++ src/main/java/HW_040923/Main.java
@@ -21,7 +21,7 @@ public class Main {
         List<Product> shoes = shopManager.getProductsByCategory(ProductCategory.SHOES);
         System.out.println("Products in category SHOES: ");
         for (Product product : shoes) {
-            System.out.println(product.getName());  // Ничего не выводит! Пробовала, не нашла причину.
+            System.out.println(product.getName());
         }
 
 
